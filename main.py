d4feb43612ee6034ab3bb6cd27495fe0fb0f0bba
Jacob Savel'ev <g6h6m238929@gmail.com>
2024-02-07 16:24:35 +0500

    Version 1.2

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from os.path import exists
-from asyncio import run
-from aiohttp.web import Response, RouteTableDef, Request, Application, R
+from aiohttp.web import Response, RouteTableDef, Request, Application, HTTPFound, run_app
 
 app = Application()
 route = RouteTableDef()
+
+def auto_check(path:str) -> str:
+    print(path, path.split('.')[-1])
+    match path.split('.')[-1]:
+        case 'css':
+            return 'text/css'
+        case 'html':
+            return 'text/html'
+        case 'js':
+            return 'text/javascript'
+        case _:
+            return 'text/txt'
 
 def get_file(filename:str) -> str:
     with open(filename, 'r', encoding='utf-8') as file:
@@ -11,4 +22,18 @@ def get_file(filename:str) -> str:
 
 @route.get('/')
 async def main_page(request:Request):
-    return
+    return HTTPFound('/main.html')
+
+@route.get('/{urlpath}')
+async def get(request:Request) -> Response:
+    url = str(request.url)
+    fl = './html/'+url.split('/', 3)[-1]
+    print(fl)
+    if exists(fl):
+        return Response(body=get_file(fl), content_type=auto_check(fl))
+    else:
+        return Response(status=404)
+
+if __name__ == '__main__':
+    app.add_routes(route)
+    run_app(app, host='localhost', port=80)
